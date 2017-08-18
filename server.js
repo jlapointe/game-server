961460c8853204ef4be443c4a043b2202d2bdbee
Jordan Lapointe <jordan.lapointe@gmail.com>
2017-08-17 21:10:03 -0400

    set CORS headers

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const app = require('express')();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const Backbone = require('backbone-lodash');
